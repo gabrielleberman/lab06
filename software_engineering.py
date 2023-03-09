6666fae84a5b8a4c08cf749665c556ebbc20a16f
rsald <r.saldajeno@ufl.edu>
2023-03-09 16:00:39 -0500

    added decoder

--- software_engineering.py
+++ software_engineering.py
@@ -4,24 +4,34 @@ def encoder(password):
     new_password = ''.join(map(str,encoded))
     return new_password
 
+def decoder(password):
+    decoded = [int(num) - 3 for num in password]
+    old_password = ''.join(map(str, decoded))
+    return old_password
+
 
 
 def main():
 
-    print('Welcome to the encoder/decoder.')
 
     while True:
-        print('1. Encode\n2. Decode\n3. Exit')
+        print('Menu\n-------------')
+        print('1. Encode\n2. Decode\n3. Quit\n')
 
-        menu_option = int(input('\nSelect menu option: '))
+        menu_option = int(input('\nPlease enter an option: '))
 
         if menu_option == 3:
             break
 
         if menu_option == 1:
             password = input('Enter password to be encoded: ')
-            print(encoder(password))
-            print()
+            encodedpass = (encoder(password))
+            print("Your password has been encoded and stored!\n")
+
+        if menu_option == 2:
+            print("The encoded password is " + encodedpass + ", and the original password is " + decoder(encodedpass) + ".\n")
+
+
 
 
 
